5c8fd0933273d07f910a94717f9d66f1248ba536
Sakshi <139939188+sakshi900600@users.noreply.github.com>
2025-08-16 12:50:54 +0530

    ques no 3 and 4 done.

--- Array/03_find kth max and min elem in array.py
+++ Array/03_find kth max and min elem in array.py
@@ -8,10 +8,40 @@ def findk_max_min(a,k):
     return [mini,maxi]
 
 
+
+# without using inbuilt function
+def findk_min(a,k):
+    
+    mini = a[0]
+    for _ in range(k):
+        mini = min(a)
+        a.remove(mini)
+    
+    return mini
+    
+def findk_max(a,k):
+    
+    maxi = a[0]
+    for _ in range(k):
+        maxi = max(a)
+        a.remove(maxi)
+    
+    return maxi
+    
+
+
+
 # input:
 a = [7, 10, 4, 3, 20, 15]
-k = 3
+k = 2
 # output:
 # [7, 10]
 
-print(findk_max_min(a,k))
+# print(findk_max_min(a,k))
+
+print(findk_min(a,k))
+print(findk_max(a,k))
+
+
+
+        
---
+++ Array/04_sort array of 0,1,2.py
@@ -0,0 +1,33 @@
+def sortarr(a):
+    left = 0
+    mid = 0
+    right = len(a)-1
+
+    while mid <= right:
+        if a[mid] == 0:
+            temp = a[left]
+            a[left] = a[mid]
+            a[mid] = temp
+            left += 1
+            mid += 1
+        
+        elif a[mid] == 1:
+            mid += 1
+        
+        else:
+            temp = a[right]
+            a[right] = a[mid]
+            a[mid] = temp
+            right -= 1
+
+    return a
+
+
+
+
+# input:
+a = [0, 1, 2, 0, 1, 2]
+# output:
+# a = [0,0,1,1,2,2]
+
+print(sortarr(a))
