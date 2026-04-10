ed141fad751448e6393e7c2618c29acbacb7fd31
Sakshi <139939188+sakshi900600@users.noreply.github.com>
2026-04-10 14:37:52 +0530

    todays ques...

--- 04_Searching & sorting/19_Bishu and Soldiers.py
+++ 04_Searching & sorting/19_Bishu and Soldiers.py
@@ -22,3 +22,37 @@ class Solution:
         return cnt
                 
 
+    # optimized
+    def killinSpree_opt(self, n):
+        # code here
+        low = 0
+        high = 10**12
+        
+        while low <= high:
+            mid = (low+high)//2
+            
+            val = self.squareSeries(mid)
+            
+            if val <= n:
+                ans = mid
+                low = mid+1
+            else:
+                high = mid-1
+        
+        return ans
+        
+    
+    def squareSeries(self, n):
+        return (n*(n+1)*(2*n+1))//6
+        
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    n = 14
+    print(sol.killinSpree(n))
+    print(sol.killinSpree_opt(n))
+
+    # Output: 3
